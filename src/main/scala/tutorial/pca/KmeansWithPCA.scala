32af70df0cf5d53d571f549dc87547c39f3c53c4
ParsianPc <saharahsani.1227@gmail.com>
2021-04-13 00:30:04 +0430

    add PCA and K-means

--- src/main/scala/tutorial/pca/KmeansWithPCA.scala
+++ src/main/scala/tutorial/pca/KmeansWithPCA.scala
@@ -11,7 +11,7 @@ object KmeansWithPCA {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
-    val conf = new SparkConf().setMaster("local[*]").setAppName("streaming")
+    val conf = new SparkConf().setMaster("local[*]").setAppName("PCATest")
       .set("spark.ui.enabled", "True")
       .set("spark.ui.port", "4040")
     val sc = new SparkContext(conf)
