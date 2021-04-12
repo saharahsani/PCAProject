9c8b8b0858fb35eebf434e1da072a6c215559b6e
ParsianPc <saharahsani.1227@gmail.com>
2021-04-13 03:35:01 +0430

    init

---
+++ src/main/scala/tutorial/pca/streaming/Main.scala
@@ -0,0 +1,49 @@
+package tutorial.pca.streaming
+
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+    val conf = new SparkConf().setMaster("local[*]").setAppName("streaming")
+      .set("spark.streaming.kafka.maxRatePerPartition", "2000")
+      .set("spark.ui.enabled", "True")
+      .set("spark.ui.port", "4040")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+
+    val ssc = new StreamingContext(sc, Seconds(1))
+    /**
+     * Reading Data From Kafka
+     * kafka consumer
+     */
+    val topics = Seq("cluTest7")
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "test",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    val streamData = stream.map(x => Vectors.dense(x.value().split(",").map(_.toDouble)))
+    val model = new OnlineClustering()
+    model.Run(streamData)
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
